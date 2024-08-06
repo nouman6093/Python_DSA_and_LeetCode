3f9c89fc3666f454f8b91137d520decf0a64085b
Nouman Hameed <62693237+nouman6093@users.noreply.github.com>
2024-08-07 01:34:25 +0500

    Create dfs_in_order.py

---
+++ dfs_in_order.py
@@ -0,0 +1,59 @@
+class Node():
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BinarySearchTree():
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        new_node = Node(value)
+        if self.root is None:
+            self.root = new_node
+            return True
+        else:
+            temp = self.root
+            while True:
+                if new_node.value == temp.value:
+                    return False
+                elif new_node.value < temp.value:
+                    if temp.left is not None:
+                        temp = temp.left
+                    else:
+                        temp.left = new_node
+                        return True
+                elif new_node.value > temp.value:
+                    if temp.right is not None:
+                        temp = temp.right
+                    else:
+                        temp.right = new_node
+                        return True
+
+    def contains(self, value):
+        if self.root is None:
+            return False
+        else:
+            temp = self.root
+            while temp is not None:
+                if value < temp.value:
+                    temp = temp.left
+                elif value > temp.value:
+                    temp = temp.right
+                else:
+                    return True
+        return False    #it means that value doesn't exist
+
+    def dfs_in_order(self):
+        results = []
+        def traverse(current_node):
+            if current_node.left is not None:
+                traverse(current_node.left)
+            results.append(current_node.value)
+            if current_node.right is not None:
+                traverse(current_node.right)
+        traverse(self.root)
+        return results
+
+t1 = BinarySearchTree()
